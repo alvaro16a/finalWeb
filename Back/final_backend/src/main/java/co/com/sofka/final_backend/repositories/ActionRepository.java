ceaced02820350a30f26eec3fe50302aef3e6ef8
Alvaro Mena <alvaro16a@gmail.com>
2022-02-24 10:56:36 -0500

    Se creo ActionRepository el repositorio de la clase Action

---
+++ Back/final_backend/src/main/java/co/com/sofka/final_backend/repositories/ActionRepository.java
@@ -0,0 +1,8 @@
+package co.com.sofka.final_backend.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import co.com.sofka.final_backend.models.*;
+
+public interface ActionRepository extends CrudRepository<Action, Long>{
+    
+}
