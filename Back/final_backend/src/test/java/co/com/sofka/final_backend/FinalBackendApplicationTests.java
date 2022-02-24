bc2139c0bab76feda7acabacf2032b1677d86190
Alvaro Mena <alvaro16a@gmail.com>
2022-02-24 10:43:41 -0500

    Se creo el proyecto del back con springboot

---
+++ Back/final_backend/src/test/java/co/com/sofka/final_backend/FinalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.sofka.final_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
